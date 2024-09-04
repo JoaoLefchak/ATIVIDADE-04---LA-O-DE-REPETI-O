c4599021dfec8b0b499cc4a57fc0e6f0de75d682
Lefchakk <joao.lefchak@edu.unipar.br>
2024-09-04 20:39:20 -0300

    Atividade 1: Inicialização e Acesso a Elementos do Vetor

---
+++ src/OperaçõesSimplesElementosVetor.java
@@ -0,0 +1,14 @@
+public class OperaçõesSimplesElementosVetor {
+    public static void main(String[] args) {
+       int[] vetor = new int[4];
+       vetor[0] = 10;
+       vetor[1] = 20;
+       vetor[2] = 30;
+       vetor[3] = 40;
+
+
+       int soma = vetor[3] + vetor[1];
+        System.out.println("Soma: " + soma);
+
+    }
+}
